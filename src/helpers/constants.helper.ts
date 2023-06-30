ae5259b30842993f65a69b542c8e7b433b811721
LexxXell <lexxxell007@gmail.com>
2023-06-30 18:52:40 +0400

    Headless fixed. FormularType env added

--- src/helpers/constants.helper.ts
+++ src/helpers/constants.helper.ts
@@ -2,4 +2,5 @@ import { FormularTypes } from '../quotasChecker/@types';
 
 export const reglistPath: string = 'storage/reglist.xlsx';
 export const regCooldown: number = Number(process.env.REG_COOLDOWN_MS) || 3600000; // def. 1 hour
-export const formularType: keyof typeof FormularTypes = 'ART8_81__10_27';
+export const formularType: keyof typeof FormularTypes =
+  (process.env.FORMULAR_TYPE as keyof typeof FormularTypes) || 'ART8_81__10_27';
