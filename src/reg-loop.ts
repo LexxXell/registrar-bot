4aad31b5583cfa264352de956b79831195523ba1
LexxXell <lexxxell007@gmail.com>
2023-08-10 18:33:41 +0400

    Fix manual restart on empty list bug

--- src/reg-loop.ts
+++ src/reg-loop.ts
@@ -33,74 +33,81 @@ export async function regLoop() {
   if (!/true/.test(process.env[regLoopAwaiting])) {
     return;
   }
-  process.env[regLoopAwaiting] = 'false';
-  logger.log('New Reg loop');
-  const persons = await PersonModel.find({ registration_number: { $eq: null }, error: { $eq: false } });
-  if (!persons.length) {
-    logger.log('Registration list is empty');
-    return;
-  }
-  logger.log(`Persons on the registration list: ${persons.length}`);
-  logger.log('Check quotas');
-  const dateQuotas: Array<DateQuotas> = await getDateQuotas(formularType);
-  if (dateQuotas.length) {
-    logger.log('Quotas available');
-    await informOnQuotas(dateQuotas);
-    const propDateQoutas = await waitForProposeDate(dateQuotas);
-    const index = dateQuotas.findIndex((item) => item.date === propDateQoutas.date);
-    if (index !== -1 && index !== 0) {
-      const [item] = dateQuotas.splice(index, 1);
-      dateQuotas.unshift(item);
+  try {
+    process.env[regLoopAwaiting] = 'false';
+    logger.log('New Reg loop');
+    const persons = await PersonModel.find({ registration_number: { $eq: null }, error: { $eq: false } });
+    if (!persons.length) {
+      logger.log('Registration list is empty');
+      process.env[regLoopAwaiting] = 'true';
+      return;
     }
-    logger.log('Start registration process');
-    dateQuotaLoop: for (let dateQuota of dateQuotas) {
-      const date = dateQuota.date;
-      let quota = dateQuota.quotas;
-      for (quota; quota > 0; quota--) {
-        if (!persons.length) {
-          break dateQuotaLoop;
-        }
-        if (quota < 20) {
-          quota = await getAvailableQuotas(formularType, date);
-          if (!quota) {
+    logger.log(`Persons on the registration list: ${persons.length}`);
+    logger.log('Check quotas');
+    const dateQuotas: Array<DateQuotas> = await getDateQuotas(formularType);
+    if (dateQuotas.length) {
+      logger.log('Quotas available');
+      await informOnQuotas(dateQuotas);
+      const propDateQoutas = await waitForProposeDate(dateQuotas);
+      const index = dateQuotas.findIndex((item) => item.date === propDateQoutas.date);
+      if (index !== -1 && index !== 0) {
+        const [item] = dateQuotas.splice(index, 1);
+        dateQuotas.unshift(item);
+      }
+      logger.log('Start registration process');
+      dateQuotaLoop: for (let dateQuota of dateQuotas) {
+        const date = dateQuota.date;
+        let quota = dateQuota.quotas;
+        for (quota; quota > 0; quota--) {
+          if (!persons.length) {
             break dateQuotaLoop;
           }
-        }
-        const person = persons.shift();
-        person.error = true;
-
-        try {
-          const regResult = await register({
-            ...person.toJSON(),
-            tip_formular: FormularTypes[formularType],
-            date,
-          });
-
-          if (regResult.date && regResult.ticket_number) {
-            person.registration_date = regResult.date;
-            person.registration_number = regResult.ticket_number;
-            person.error = false;
+          if (quota < 20) {
+            quota = await getAvailableQuotas(formularType, date);
+            if (!quota) {
+              break dateQuotaLoop;
+            }
           }
+          const person = persons.shift();
+          person.error = true;
+
+          try {
+            const regResult = await register({
+              ...person.toJSON(),
+              tip_formular: FormularTypes[formularType],
+              date,
+            });
+
+            if (regResult.date && regResult.ticket_number) {
+              person.registration_date = regResult.date;
+              person.registration_number = regResult.ticket_number;
+              person.error = false;
+            }
+          } catch (e) {
+            logger.error(e);
+          }
+
+          await person.save();
+        }
+      }
+      if ((await PersonModel.find({ $or: [{ registration_number: { $ne: null } }, { error: true }] })).length) {
+        logger.log('Send registration info');
+        await sendRegistrationDetails();
+        try {
+          logger.log('Cleaning registered and error persons');
+          await PersonModel.deleteMany({ $or: [{ registration_number: { $ne: null } }, { error: true }] });
         } catch (e) {
+          sendErrorToAdmin(`DELETION PERSONS ERROR\n${(e as Error).message}`);
           logger.error(e);
         }
-
-        await person.save();
       }
-    }
-    if ((await PersonModel.find({ $or: [{ registration_number: { $ne: null } }, { error: true }] })).length) {
-      logger.log('Send registration info');
-      await sendRegistrationDetails();
-      try {
-        logger.log('Cleaning registered and error persons');
-        await PersonModel.deleteMany({ $or: [{ registration_number: { $ne: null } }, { error: true }] });
-      } catch (e) {
-        sendErrorToAdmin(`DELETION PERSONS ERROR\n${(e as Error).message}`);
-        logger.error(e);
-      }
-    }
 
-    logger.log('Finish registration process');
+      logger.log('Finish registration process');
+      process.env[regLoopAwaiting] = 'true';
+    }
+  } catch (e) {
+    logger.error(e);
+    sendErrorToAdmin((e as Error).message);
     process.env[regLoopAwaiting] = 'true';
   }
 }
